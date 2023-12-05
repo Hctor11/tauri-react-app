cda0b93ab425aad8b2a13cdc40041990cc561e59
hector <hectorrrivera1010@gmail.com>
2023-12-04 23:35:30 -0600

    [add] enabled patj

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -1,12 +1,20 @@
+import { writeTextFile } from "@tauri-apps/api/fs";
+import { desktopDir } from "@tauri-apps/api/path";
+
 const SnippetForm = () => {
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault()
-      alert('saved form')
-    }}>
-      <input type="text" 
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const desktopPath = await desktopDir();
+        writeTextFile(desktopPath);
+      }}
+    >
+      <input
+        type="text"
         placeholder="Type snippet name.."
-        className="p-1 border-2 border-slate-300 rounded-md text-xs w-full" />
+        className="p-1 border-2 border-slate-300 rounded-md text-xs w-full"
+      />
     </form>
   );
 };
